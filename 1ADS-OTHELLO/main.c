b70c95a18d7d7c63f8256c1ed5f65be6ce3d641e
Marion Savaresse <marionsavaresse@mbp-de-marion.home>
2014-01-25 14:30:30 +0100

    Initial Commit

---
+++ 1ADS-OTHELLO/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  1ADS-OTHELLO
+//
+//  Created by Marion Savaresse on 25/01/2014.
+//  Copyright (c) 2014 Marion Savaresse. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
